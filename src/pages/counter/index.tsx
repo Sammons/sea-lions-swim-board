a243357f8bfc2fcf650f666869647c5f983a7bc7
canadianator <zorozco@ztechaerial.com>
2024-06-10 21:57:12 -0500

    index.tsx

--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -6,8 +6,8 @@ import { Counter, FormatCount } from '../../data/counts'
 import background from '../../images/sealionlogo.png';
 
 const CounterDiv = styled('div')`
-  height: 120vh;
-  width: 115vw;
+  height: 105vh;
+  width: 100vw;
   border-radius: 17vh;
   display: flex;
   align-items: center;
