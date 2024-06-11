3a0bf0ac7d2f0e09d15006e90ae84efd22974da0
canadianator <zorozco@ztechaerial.com>
2024-06-10 22:13:15 -0500

    index.tsx

--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -17,14 +17,14 @@ const CounterDiv = styled('div')`
 
 const CounterNumber = styled('div')`
   color: white;
-  font-size: 40vh;
+  font-size: 50vh;
   width: 100%;
   text-align: center;
 `
 
 const CounterLabel = styled('div')`
   color: white;
-  font-size: 3.5vw;
+  font-size: 5.5vw;
   width: 100%;
   text-align: center;
 `
